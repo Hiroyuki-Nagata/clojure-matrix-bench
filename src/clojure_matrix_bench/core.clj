562cca13e5a019892f08b671432d46c5a6b3e927
hiroyuki nagata <newserver002@gmail.com>
2017-04-13 22:40:50 +0900

    Add ojAlgo

--- src/clojure_matrix_bench/core.clj
+++ src/clojure_matrix_bench/core.clj
@@ -1,7 +1,11 @@
 (ns clojure-matrix-bench.core
   (:gen-class main)
-  (:require [clojure-matrix-bench.clatrix :as clatrix]))
+  (:require [clojure-matrix-bench.clatrix :as clatrix]
+            [clojure-matrix-bench.ojalgo :as ojalgo]))
 
 (defn -main [& {:as args}]
   (println "start => clatrix 0.5.0")
-  (clatrix/testing))
+  (clatrix/testing)
+  (println "start => ojalgo 42.0.0")
+  (ojalgo/testing)
+  )
--- src/clojure_matrix_bench/ojalgo.clj
+++ src/clojure_matrix_bench/ojalgo.clj
@@ -1,8 +1,15 @@
 (ns clojure-matrix-bench.ojalgo
-  (:import (org.ojalgo.matrix.BigMatrix)))
+  (:import (org.ojalgo.matrix BigMatrix BasicMatrix)))
 
 (defn test-main []
-  )
+  (let [n 3000
+        f (. BigMatrix FACTORY)
+        b (. f getBuilder n n)
+        m (. b build)]
+    ;; (println (. m countColumns))
+    ;; (println (. m countRows))
+    ;; (println m)
+    ))
 
 (defn testing []
-  )
+  (time (test-main)))
